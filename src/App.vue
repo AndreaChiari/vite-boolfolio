1f92e8a42d0147df7671b2062def44985dfe85d7
AndreaChiari <chiariandrea94@gmail.com>
2023-03-15 15:02:29 +0100

    correct-API-call

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import AppCard from './components/AppCard.vue';
 export default {
   name: 'App',
-  components: { AppHeader }
+  components: { AppHeader, AppCard }
 }
 </script>
 
 <template>
   <AppHeader></AppHeader>
+  <main class="container-fluid">
+    <AppCard></AppCard>
+  </main>
 </template>
--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,10 +1,22 @@
 <script>
+import axios from 'axios';
+const apiBaseUrl = 'http://127.0.0.1:8000/api/'
 export default {
     name: 'CardApp',
     data: () => ({
         projects: []
     }),
-};
+    methods: {
+        fetchProjects() {
+            axios.get(apiBaseUrl + 'projects').then(res => {
+                console.log(res.data);
+            })
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
+}
 </script>
 
 <template></template>
