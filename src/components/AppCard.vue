97da079060b551ecd6c045bdd369bbff106fc14c
AndreaChiari <chiariandrea94@gmail.com>
2023-03-15 16:08:44 +0100

    esercizio-completo

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -8,19 +8,18 @@ export default {
             const abstract = this.project.description.slice(0, 100)
             return abstract + '...';
         }
-
     }
 };
 </script>
 
 <template>
-    <div class="card col-4 g-5">
-        <img class="card-img-top" src="{{ project.image }}" alt="Card image cap">
+    <div class="card col-4 g-3">
+        <img class="card-img-top" :src="project.image" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">{{ project.name }}</h5>
             <p class="card-text">{{ abstract }}</p>
-            <a href="" class="btn btn-primary me-4">{{ project.linkedin }}</a>
-            <a href="" class="btn btn-primary">{{ project.github }}</a>
+            <a :href="project.linkedin" class="btn btn-primary mb-4">{{ project.linkedin }}</a>
+            <a :href="project.github" class="btn btn-primary">{{ project.github }}</a>
         </div>
     </div>
 </template>
